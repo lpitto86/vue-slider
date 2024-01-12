c7e519b62b25cdcd34681276e31538535e4bc020
lpitto86 <luca.p481@gmail.com>
2024-01-12 19:16:58 +0100

    Immagini

--- js/script.js
+++ js/script.js
@@ -1,25 +1,34 @@
-const slides = [
-{
-    image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            images: ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04.webp', 'img/05.webp'],
+            currentIndex: 0
+        };
+    },
+    methods: {
+        showImage(index) {
+            this.currentIndex = index;
 
-console.log(slides);
+            // Auto play logic
+            clearInterval(this.autoplayInterval);
+            this.autoplayInterval = setInterval(this.autoPlay, 3000);
+        },
+        prevImage() {
+            const previousIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+            this.showImage(previousIndex);
+        },
+        nextImage() {
+            const nextIndex = (this.currentIndex + 1) % this.images.length;
+            this.showImage(nextIndex);
+        },
+        autoPlay() {
+            const nextIndex = (this.currentIndex + 1) % this.images.length;
+            this.showImage(nextIndex);
+        }
+    },
+    mounted() {
+        // Auto play on mount
+        this.autoplayInterval = setInterval(this.autoPlay, 3000);
+    }
+});
